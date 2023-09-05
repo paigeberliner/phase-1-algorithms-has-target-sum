7aa05a68cd80f75df2645ce9e7aa93441cee7ff6
Paige <paigeberliner@gmail.com>
2023-09-05 13:56:17 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
   Add your pseudocode here
-*/
+1. create a function hasTargetSum()
+   Two arugements: 
+   Array of integers 
+   Target integer 
+
+2. if array [i] + array [i+1] (as long as i+1 > array.length) === target
+return true 
+else
+return false 
 
 /*
-  Add written explanation of your solution here
+  let i = each element in the array 
+  let j = each element in the array + 1 
+  i+j = sum of each elmeent in the array 
 */
 
 // You can run `node index.js` to view these console logs
